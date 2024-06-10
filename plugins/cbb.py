b092d3a4de1fa0db572bbc67fba2aaaed7bb9252
SanjayMovie <137784079+SanjayMovie@users.noreply.github.com>
2024-06-10 13:00:39 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={1471883657}'>This Person</a>\n○ Language : <code>Telugu</code>\n○ Library : <a href=''>Pyrogram asyncio {__version__}</a>\n○ Request Group: <a href='https://t.me/Movies_Request_Group_One'>Click here</a>\n○ Channel : @Telugu_Movie_Room\n○ Support Group : @Movies_Request_Group_Two</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
